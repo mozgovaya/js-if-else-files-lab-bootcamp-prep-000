1fc4778771dfda573e4a93b441478789d73f450f
Natalia Mozgovaya <mozgovaya@gmail.com>
2019-10-27 16:26:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var name = "Joe";
 var height = "74"
 
-if (name === "Susan")   {
+if (name === "Joe")   {
   console.log(
     "The name is correct")
 }
@@ -9,6 +9,10 @@ if (name === "Susan")   {
 else {
   console.log(
     "Expected the name to be Susan")
+}
+
+if (height === "74")  {
+  console.log("The height is correct")
 }
 
 // Don't worry about this
